dfa67a579c06b74964c775772daa59c342b024c5
Cleo35 <86663165+Cleo35@users.noreply.github.com>
2023-03-18 04:27:08 +0200

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chat-rgv0.onrender.com/', {
+  const response = await fetch('https://chat-rgv0.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
